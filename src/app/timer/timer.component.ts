acb79e3fb184588ab0df5c30065bb3db2ef66f60
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-12 14:16:23 -0500

    all done

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -8,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class TimerComponent implements OnInit {
 
   keepingTime: boolean = false;
+
   timerMinutes: number = 25;
+  
   timerStarted: boolean = false;
-  timerMessageStart: string = "Start your timer!"
-  timerMessageStop: string = "Stop your timer!"
+
+  timerMessageStart: string = "Start your timer!";
+
+  timerMessageStop: string = "Stop your timer!";
+
+  revealAbout: boolean = false;
+
+  timer = setInterval(() => {
+    if (this.timerStarted && this.timerMinutes !== 0) this.timerMinutes--;
+    if (this.timerMinutes === 0) {
+      clearInterval(this.timer);
+      alert('Time expired!');
+    }
+  }, 1000);
+  
 
   timerMode() {
     this.keepingTime = !this.keepingTime;
@@ -24,11 +39,18 @@ export class TimerComponent implements OnInit {
   }
 
   onStartTimer() {
+    this.timer;
     this.timerMode();
   }
 
   onResetTimer() {
-    this.timerMode();
+    confirm("You sure you wanna reset?");
+    if (this.keepingTime) {
+      this.timerMode();
+      this.timerMinutes = 25;
+    } else {
+      this.timerMinutes = 25;
+    }
   }
 
 }
