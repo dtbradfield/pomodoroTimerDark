df4d6ec13a2da8d03d1f2391ea06d1081229de25
DonTheChimera <donaldtbradfield@gmail.com>
2019-03-12 11:23:53 -0500

    buttons with dynamic button messages created successfully

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TimerComponent implements OnInit {
 
+  keepingTime: boolean = false;
+  timerMinutes: number = 25;
+  timerStarted: boolean = false;
+  timerMessageStart: string = "Start your timer!"
+  timerMessageStop: string = "Stop your timer!"
+
+  timerMode() {
+    this.keepingTime = !this.keepingTime;
+    this.timerStarted = !this.timerStarted;
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartTimer() {
+    this.timerMode();
+  }
+
+  onResetTimer() {
+    this.timerMode();
   }
 
 }
